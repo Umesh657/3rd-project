ba8266f63e8d3e4ce5dc2ef18bc52302b8a2da16
Kamlesh <umeshdongre6m@gmail.com>
2024-05-21 10:15:55 +0530

    create

---
+++ SwpTwoNumber.java
@@ -0,0 +1,18 @@
+package cod.codeo.swap;
+
+public class SwpTwoNumber {
+	public static void main (String[]args) {
+	int a=5;
+	int b=3;
+    System.out.println("Before swaping A : "+a);
+    System.out.println("Before swaping B : "+b);
+    int c=0;
+    c=a;
+    a=b;
+    b=c;
+    System.out.println("After swaping A : "+a);
+    System.out.println("After swaping B: "+b);
+    
+
+}
+}
